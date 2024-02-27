d158581be3907b04951ffe8a8af8ceabaa9c37e1
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-02-28 00:50:01 +0600

    commit.01

--- TTT.java
+++ TTT.java
@@ -1,4 +1,3 @@
-// A simple program to demonstrate 
 // Tic-Tac-Toe Game.
 import java.util.*;
 
@@ -6,11 +5,6 @@ public class TTT {
 
 	static String[] board;
 	static String turn;
-
-
-	// CheckWinner method will 
-	// decide the combination 
-	// of three box given below.
 	static String checkWinner()
 	{
 		for (int a = 0; a < 8; a++) {
@@ -42,12 +36,10 @@ public class TTT {
 				line = board[2] + board[4] + board[6];
 				break;
 			}
-			//For X winner
 			if (line.equals("XXX")) {
 				return "X";
 			}
 			
-			// For O winner
 			else if (line.equals("OOO")) {
 				return "O";
 			}
@@ -62,23 +54,11 @@ public class TTT {
 				return "draw";
 			}
 		}
-
-	// To enter the X Or O at the exact place on board.
 		System.out.println(
 			turn + "'s turn; enter a slot number to place "
 			+ turn + " in:");
 		return null;
 	}
-	
-	// To print out the board.
-	/* |---|---|---|
-	| 1 | 2 | 3 |
-	|-----------|
-	| 4 | 5 | 6 |
-	|-----------|
-	| 7 | 8 | 9 |
-	|---|---|---|*/
-
 	static void printBoard()
 	{
 		System.out.println("|---|---|---|");
@@ -115,11 +95,6 @@ public class TTT {
 
 		while (winner == null) {
 			int numInput;
-		
-		// Exception handling.
-		// numInput will take input from user like from 1 to 9.
-		// If it is not in range from 1 to 9.
-		// then it will show you an error "Invalid input."
 			try {
 				numInput = in.nextInt();
 				if (!(numInput > 0 && numInput <= 9)) {
@@ -133,9 +108,6 @@ public class TTT {
 					"Invalid input; re-enter slot number:");
 				continue;
 			}
-			
-			// This game has two player x and O.
-			// Here is the logic to decide the turn.
 			if (board[numInput - 1].equals(
 					String.valueOf(numInput))) {
 				board[numInput - 1] = turn;
@@ -155,15 +127,10 @@ public class TTT {
 					"Slot already taken; re-enter slot number:");
 			}
 		}
-	
-		// If no one win or lose from both player x and O.
-		// then here is the logic to print "draw".
 		if (winner.equalsIgnoreCase("draw")) {
 			System.out.println(
 				"It's a draw! Thanks for playing.");
 		}
-	
-		// For winner -to display Congratulations! message.
 		else {
 			System.out.println(
 				"Congratulations! " + winner
